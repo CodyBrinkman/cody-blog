d5bc441391ec59e75ba23dd4f4b3b4c8bbf8125a
Cody Brinkman <codybrinkmansoccer@gmail.com>
2021-12-16 11:12:32 -0500

    changing path

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import blogService from './services/blogs'
+import blogService from '../services/blogs'
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([])
