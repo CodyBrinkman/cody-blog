77cf88f55a7dd51f1090e9da7339dfdc01948191
Cody Brinkman <codybrinkmansoccer@gmail.com>
2021-12-16 10:27:19 -0500

    adding blog service

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import blogService from './services/blogs'
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(response => {
+      setBlogs(response.data)
+      console.log('blogsss' + response.data)
+    })
+  }, [])
 
   return (
     <h1>Blogs</h1>
