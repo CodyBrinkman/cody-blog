bce313a2c968b0b5f6c4e5d592617790b68401d9
Cody Brinkman <codybrinkmansoccer@gmail.com>
2021-12-17 17:21:54 -0500

    react markdown

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import blogService from '../services/blogs'
+import ReactMarkdown from 'react-markdown'
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([])
@@ -16,7 +17,7 @@ const Blogs = () => {
     <>
       <h1>Blogs</h1>
       <ul>
-        {blogs.map(blog => <li key={blog.id}>{blog.id} {MD}</li>)}
+        {blogs.map(blog => <li key={blog.id}>{blog.id} <ReactMarkdown>{MD}</ReactMarkdown></li>)}
       </ul>
     </>
 
