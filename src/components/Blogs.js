bce6313742eec2d72cf6bc3893b37e0524b0f1fa
Cody Brinkman <codybrinkmansoccer@gmail.com>
2021-12-25 15:51:14 -0500

    xmas

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,19 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 const Blogs = ({ blogs }) => {
 
   return (
     <>
       <h1>Blogs</h1>
-      <ul>
+      <CardGroup>
+      <Container>
+      <Row>
       {blogs.map((blog, i) =>
-        <div key={i}>
-          <Link to={`/blogs/${blog.filename}`}>{blog.title}</Link>
-        </div>
+        <Col>
+        <Card key={i} style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require(`../images/${blog.filename}.png`)} thumbnail/>
+          <Card.Body>
+            <Card.Title>{blog.title}</Card.Title>
+            <Card.Text>
+              {blog.description}
+            </Card.Text>
+            <Link to={`/blogs/${blog.filename}`}>
+              <Button variant="primary">Read Blog</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+        </Col>
       )}
-      </ul>
+      </Row>
+      </Container>
+      </CardGroup>
     </>
 
   )
