59c8e8427e6e957aca1c4af50fc3405c986eab2b
Cody Brinkman <codybrinkmansoccer@gmail.com>
2022-01-10 12:02:14 -0500

    new

--- src/components/DIYBlogs/JewelryAntlers.js
+++ src/components/DIYBlogs/JewelryAntlers.js
@@ -8,22 +8,38 @@ const JewelryAntlers = () => {
       <div style={{paddingLeft: 10, paddingRight: 10}}>
         <h1>Deer Antler Jewelry Holder</h1>
         <br />
-        <div style={{fontSize: 18}}>
+        <div style={{fontSize: 22}}>
           This is one of those builds that started off with a simple idea and bloomed into something better than I could have ever imagined. And the more I do these builds, the more I find this happens. The lesson being: <strong>just get started, the creativity and ideas will reveal themselves along the way!</strong>
           <br />
           <br />
           <h4>1. The idea</h4>
-          This initial idea I had was a jewely holder. From there I Googled "unique jewelry holder" and saw a really cool picture of someone using a tree branch to hang necklaces and bracelets off of. I was originally going to use this method, but then I saw a tree branch that reminded me a lot like an old deer antler I found as a kid and we have stored in my garage forever. And boom, idea tranformation #1: instead of a tree branch I would screw a single deer antler into a piece of wood.
+          This initial idea I had was a jewely holder. From there I Googled "unique jewelry holder" and saw a cool picture of someone using a tree branch to hang necklaces and bracelets off of. I was originally going to use this method, but then I saw a tree branch that reminded me a lot like an old deer antler I found as a kid and we have stored in my garage forever. And boom, idea tranformation #1: instead of a tree branch I would screw a single deer antler into a piece of wood.
           <br />
           <br />
-          I do not live in that home anymore so I immediately texted my dad to go into the garage and grab the antler for me. To my dismay, he checked high and low and could not find it. Little did I know this would be the best possible thing for this build. After expressing my disappointed it had been thrown away since I was gone, my mom let me know she works with somebody whose family are large hunters and she would ask if they had extra antlers laying around. Not only did they have extra antlers, but they had antlers still on the skull plate! And boom, idea transformation #2: instead of using a single antler I would use antlers still on the skull plate. Now I know the build was going to get a lot more involved as I have never done anything close to mounting antlers before. I used my best friends Google and Youtube and came across <a href='https://www.youtube.com/watch?v=9J6H5NMsoLQ'>this</a> video by Whitebone Creations Hunting and it looked like something my dad and I could handle.
-          <Image name='startingantlers' caption='Figure 1: Starting antlers on skull plates'/>
+          I do not live in that home anymore, so I immediately texted my dad to go into the garage and grab the antler for me. To my dismay, he checked high and low and could not find it. Little did I know this would be the best possible thing for this build. After expressing my disappointed it had been thrown away since I was gone, my mom let me know she works with somebody whose family are large hunters and she would ask if they had extra antlers laying around. Not only did they have extra antlers, but they had antlers still on the skull plate! And boom, idea transformation #2: instead of using a single antler I would use antlers still on the skull plate. Now I know the build was going to get a lot more involved as I have never done anything close to mounting antlers before. I used my best friends Google and Youtube and came across <a href='https://www.youtube.com/watch?v=9J6H5NMsoLQ'>this</a> video by Whitebone Creations Hunting and it looked like something my dad and I could handle.
+          <Image name='startingantlers' caption='Figure 1: Antlers on skull plate'/>
           <br />
           <h4>2. Prepping the antlers</h4>
           As you can see in the above image most of the hide was already gone so the first thing I did was clean the pedicle which is yellow stuff you see at the base of the antler. A flathead screwdriver did the trick. After that I put some dishsoap in warm water and used an old sock to clean it all. If the above doesn't work for you just give it a Google search and you will find plenty of ways to clean yours.
           <Image name='cleaningantlers' caption='Figure 2: Prepping the antlers' />
           <br />
           <h4>3. Securing to base</h4>
+          After the antlers were sufficiently clean, I searched our garage for some scrap wood. My dad had some scrap oak from a recent project so I cut a small rectangle for my base, but any wood and any shape work because this wood will not be seen. In the video they cut some old plywood into a spade looking shape. This is one area I wish I would have done slightly different. As you can see in the below image, the base is too large. If I was doing this again, I would trim it down to more closely match the size of the skull plate. Having such a large base led to a lot of excess when forming the cement mix (as you will see in the next step).
+          <br />
+          <br />
+          Once the wood base was cut a couple screws is all it took to secure. I had my dad hold the antlers in the proper orientation, and once we had it how we liked I drilled a couple pilot holes with a drill bit, and then followed with the screws. The only thing you are going for here is holding the antlers in the proper orientation - the cement mix in the next step is what will fully secure your antlers. Also, if you accidentally crack your antlers, no worries. Just use more screws to get it held how you like, and again the cement mix will secure it all.
+          <Image name='securingtobase' caption='Figure 3: Securing antlers to scrap wood base'/>
+          <br />
+          <h4>4. Forming the cement base</h4>
+          This step is one I had pretty much no previous experience with and was a little nervous going into. The goal is to form and shape a cement base for the antlers that will hold everything together, and provide a surface for us to stretch the leather over. For this mix I followed the video and did 1 part cement and 1 part plaster of paris. I was able to buy both at my local home improvement store, The Home Depot to be specific, and it worked great. If you cannot get either of these products for some reason, I am sure a Google search will yield other mixtures that work.
+          <br />
+          <br />
+          The ratio is 1:1 between cement and plaster of paris, and the amount to mix is up to your need. Two hand scoops of each was plenty for me. After taking these equal parts, I made sure to mix it up good in the bucket before adding the water. I kept adding water until the consistency was that of clay, and then started packing it into every spot I could. I recommend using cold water which will slow down the setting time of the mix. After it was completely covering the skull plate, my dad and I began removing any excess and shaping the surface with a putty knife. Don't feel like you must rush, but keep in mind you have 3-4 minutes before the mix really starts to set. Make sure to leave room around the antler's cubicles, but other than that there's really no rhyme or reason to it - just find any tools you think will be useful and keep trimming and shaping, and then smooth it over once the shape is what you want.
+          <Image name='formingcementbase' caption='Figure 4: Forming cement and plaster of paris base'/>
+          <br />
+          As I mentioned above, my wooden base was slightly too large so my cement base was larger than it needed to be, but I am still really happy with how it turned out. And again, this is going to get covered so if it has small imperfections don't fret. I do recommend watching some online videos (specifically the one I linked above) before this step to get an idea of how it will go, but be creative and give it a shot!
+          <Image name='cementbasefinal' caption='Figure 5: Cement base outcome'/>
+          df
         </div>
       </div>
     </>
--- src/components/TechBlogs.js
+++ src/components/TechBlogs.js
@@ -5,83 +5,84 @@ import CardGroup from 'react-bootstrap/CardGroup'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
 
 const TechBlogs = () => {
 
   return (
     <>
-    <h1 style={{paddingLeft: 10}}>Technical Blogs</h1>
-    <br />
-    <CardGroup>
-    <Container>
-    <Row>
-      <Col>
-        <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src='/images/awss3.png'/>
-          <Card.Body>
-            <Card.Title>How to use AWS S3 presigned URLs with OCI Object Storage</Card.Title>
-            <Card.Text>
-              Learn how to use OCI's Amazon S3 Compatibility API to make your S3 presigned URLs
-               work with OCI Object Storage.
-            </Card.Text>
-            <a href='https://blogs.oracle.com/cloud-infrastructure/post/how-to-use-aws-s3-presigned-urls-with-oci-object-storage'>
-              <Button variant="primary">Read Blog</Button>
-            </a>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col>
-        <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src='/images/testocidns.png'/>
-          <Card.Body>
-            <Card.Title>Testing OCI DNS and Traffic Management steering policies before migration</Card.Title>
-            <Card.Text>
-              DNS changes are scary. This blog walks through an avenue to test your zone's behavior before
-              nameserver delegation to ensure a seamless migration.
-            </Card.Text>
-            <a href='https://blogs.oracle.com/cloud-infrastructure/post/testing-oci-dns-and-traffic-management-steering-policies-before-migration'>
-              <Button variant="primary">Read Blog</Button>
-            </a>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col>
-        <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src='/images/activefailover.png'/>
-          <Card.Body>
-            <Card.Title>Setting up active failover with OCI Traffic Management Steering</Card.Title>
-            <Card.Text>
-              Oracle has used its investment in Dyn to provide some of the best DNS and Edge capabilities
-              in the market, one being active failover. Active failover ensures that sites and web services
-              stay online if an issue occurs and works by automatically moving traffic off an unhealthy web
-              server to a healthy secondary web server.
-            </Card.Text>
-            <a href='https://blogs.oracle.com/cloud-infrastructure/post/setting-up-active-failover-with-oci-traffic-management-steering'>
-              <Button variant="primary">Read Blog</Button>
-            </a>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-    <br />
-    <Row>
-      <Col>
-        <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src='/images/godaddy.png'/>
-          <Card.Body>
-            <Card.Title>GoDaddy DNS management through Oracle Cloud Infrastructure</Card.Title>
-            <Card.Text>
-              Learn how to start managing your GoDaddy registered domain with OCI DNS.
-            </Card.Text>
-            <a href='https://blogs.oracle.com/cloud-infrastructure/post/how-to-manage-your-godaddy-domain-with-oci-dns'>
-              <Button variant="primary">Read Blog</Button>
-            </a>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-    </Container>
-    </CardGroup>
+      <h1 style={{paddingLeft: 10}}>Technical Blogs</h1>
+      <br />
+      <CardGroup>
+      <Container>
+      <Row>
+        <Col>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src='/images/awss3.png'/>
+            <Card.Body>
+              <Card.Title>How to use AWS S3 presigned URLs with OCI Object Storage</Card.Title>
+              <Card.Text>
+                Learn how to use OCI's Amazon S3 Compatibility API to make your S3 presigned URLs
+                 work with OCI Object Storage.
+              </Card.Text>
+              <a href='https://blogs.oracle.com/cloud-infrastructure/post/how-to-use-aws-s3-presigned-urls-with-oci-object-storage'>
+                <Button variant="primary">Read Blog</Button>
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src='/images/testocidns.png'/>
+            <Card.Body>
+              <Card.Title>Testing OCI DNS and Traffic Management steering policies before migration</Card.Title>
+              <Card.Text>
+                DNS changes are scary. This blog walks through an avenue to test your zone's behavior before
+                nameserver delegation to ensure a seamless migration.
+              </Card.Text>
+              <a href='https://blogs.oracle.com/cloud-infrastructure/post/testing-oci-dns-and-traffic-management-steering-policies-before-migration'>
+                <Button variant="primary">Read Blog</Button>
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src='/images/activefailover.png'/>
+            <Card.Body>
+              <Card.Title>Setting up active failover with OCI Traffic Management Steering</Card.Title>
+              <Card.Text>
+                Oracle has used its investment in Dyn to provide some of the best DNS and Edge capabilities
+                in the market, one being active failover. Active failover ensures that sites and web services
+                stay online if an issue occurs and works by automatically moving traffic off an unhealthy web
+                server to a healthy secondary web server.
+              </Card.Text>
+              <a href='https://blogs.oracle.com/cloud-infrastructure/post/setting-up-active-failover-with-oci-traffic-management-steering'>
+                <Button variant="primary">Read Blog</Button>
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col>
+          <Card style={{ width: '22rem' }}>
+            <Card.Img variant="top" src='/images/godaddy.png'/>
+            <Card.Body>
+              <Card.Title>GoDaddy DNS management through Oracle Cloud Infrastructure</Card.Title>
+              <Card.Text>
+                Learn how to start managing your GoDaddy registered domain with OCI DNS.
+              </Card.Text>
+              <a href='https://blogs.oracle.com/cloud-infrastructure/post/how-to-manage-your-godaddy-domain-with-oci-dns'>
+                <Button variant="primary">Read Blog</Button>
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </Container>
+      </CardGroup>
     </>
   )
 
